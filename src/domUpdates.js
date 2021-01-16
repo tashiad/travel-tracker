a24df7ba2c99db62931b14ad42a3feaf953a5896
tashiad <tashiadavis10@gmail.com>
2021-01-16 14:16:47 -0700

    Generate a random current traveler, pulled in from fetch API

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,23 @@
+const welcomeText = document.querySelector("#welcome-text")
+const cardGrid = document.querySelector(".card-grid")
+
 const domUpdates = {
 
-  domTest() {
-    console.log('dom file')
-  }
+  addWelcomeMessage(firstName) {
+    welcomeText.innerText = `Welcome ${firstName}! Here are your trips.`
+  },
+
+  // addCardToDom(recipeInfo, shortRecipeName) {
+  //   const newRecipeCard = cardTemplate.content.cloneNode(true)
+  //   newRecipeCard.querySelector('article.recipe--card').id = recipeInfo.id
+  //   newRecipeCard.querySelector('h4.recipe--title').innerText = shortRecipeName
+  //   newRecipeCard.querySelector('img.recipe--photo').src = recipeInfo.image
+  //   newRecipeCard.querySelector('img.recipe--photo').alt = recipeInfo.name
+  //   newRecipeCard.querySelector('img.recipe--photo').title = `${recipeInfo.name} recipe`
+  //
+  //   this.displayTags(recipeInfo.tags, newRecipeCard)
+  //   recipeContainer.appendChild(newRecipeCard)
+  // }
 }
 
 export default domUpdates
--- src/index.js
+++ src/index.js
@@ -6,15 +6,53 @@ import Destination from './Destination.js'
 import Trip from './Trip.js'
 
 // QUERY SELECTORS
-
+const welcomeText = document.querySelector("#welcome-text")
+const cardGrid = document.querySelector(".card-grid")
+const buttonQuote = document.querySelector("#button-quote")
+const buttonSubmit = document.querySelector("#button-submit")
 
 // EVENT LISTENERS
-
+// buttonQuote.addEventListener('click', tbd)
+// buttonSubmit.addEventListener('click', tbd)
 
 // GLOBAL VARIABLES
-
+let currentTraveler
 
 // FETCH API DATASETS
+const fetchedTravelerData = fetchApi.getTravelerData()
+const fetchedTripData = fetchApi.getTripData()
+const fetchedDestinationData = fetchApi.getDestinationData()
 
+Promise.all([fetchedTravelerData, fetchedTripData, fetchedDestinationData])
+  .then(values => {
+    generateTraveler(values[0])
+    // createRecipeDataset(values[1])
+    // addRecipeNameAndCost(values[2])
+    // loadWebsite()
+  }).catch(handleErrorMessages)
+
+// function loadWebsite() {
+//   createTripCards()
+// }
+
+function handleErrorMessages(error) {
+  window.alert('The server was not accessible at this time. Please reload the page or try again later.')
+  console.log(error)
+}
 
 // POPULATE WEBSITE INFORMATION
+function generateTraveler(travelerData) {
+  currentTraveler = new Traveler(travelerData.travelers[Math.floor(Math.random() * travelerData.travelers.length)])
+  let firstName = currentTraveler.name.split(' ')[0]
+  domUpdates.addWelcomeMessage(firstName)
+}
+
+// function findRecipe(recipeID) {
+//   return allRecipes.find(recipe => recipe.id === Number(recipeID))
+// }
+//
+// function createTripCards() {
+//   fetchedTripData.forEach(trip => {
+//     domUpdates.addCardToDom(trip)
+//   })
+// }
