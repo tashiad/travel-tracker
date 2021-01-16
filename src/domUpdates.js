5652c6fc9fdd866b064b1cb7718ca1a42d7a2c07
tashiad <tashiadavis10@gmail.com>
2021-01-16 16:01:14 -0700

    Display all traveler trip cards

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,23 +1,26 @@
-const welcomeText = document.querySelector("#welcome-text")
-const cardGrid = document.querySelector(".card-grid")
+const welcomeText = document.querySelector('#welcome-text')
+const cardGrid = document.querySelector('.card-grid')
+const cardTemplate = document.querySelector('.template-card')
 
 const domUpdates = {
-
   addWelcomeMessage(firstName) {
     welcomeText.innerText = `Welcome ${firstName}! Here are your trips.`
   },
 
-  // addCardToDom(recipeInfo, shortRecipeName) {
-  //   const newRecipeCard = cardTemplate.content.cloneNode(true)
-  //   newRecipeCard.querySelector('article.recipe--card').id = recipeInfo.id
-  //   newRecipeCard.querySelector('h4.recipe--title').innerText = shortRecipeName
-  //   newRecipeCard.querySelector('img.recipe--photo').src = recipeInfo.image
-  //   newRecipeCard.querySelector('img.recipe--photo').alt = recipeInfo.name
-  //   newRecipeCard.querySelector('img.recipe--photo').title = `${recipeInfo.name} recipe`
-  //
-  //   this.displayTags(recipeInfo.tags, newRecipeCard)
-  //   recipeContainer.appendChild(newRecipeCard)
-  // }
+  addCardToDom(trip) {
+    const newTripCard = cardTemplate.content.cloneNode(true)
+    newTripCard.querySelector('h4.card-destination').innerText = trip.destinationDetails.name
+    newTripCard.querySelector('img.card-image').src = trip.destinationDetails.image
+    newTripCard.querySelector('img.card-image').alt = trip.destinationDetails.alt
+    newTripCard.querySelector('p.card-travelers').innerHTML = `<strong>Travelers:</strong> ${trip.travelers}`
+    newTripCard.querySelector('p.card-date').innerHTML = `<strong>Start Date:</strong> ${trip.date}`
+    newTripCard.querySelector('p.card-duration').innerHTML = `<strong>Duration:</strong> ${trip.duration} days`
+    newTripCard.querySelector('p.card-lodging').innerHTML = `<strong>Lodging:</strong> $${trip.destinationDetails.lodging} per night`
+    newTripCard.querySelector('p.card-flights').innerHTML = `<strong>Flights:</strong> $${trip.destinationDetails.flights} per person`
+    newTripCard.querySelector('p.card-status').innerHTML = `<strong>Status:</strong> ${trip.status}`
+
+    cardGrid.appendChild(newTripCard)
+  }
 }
 
 export default domUpdates
--- src/index.js
+++ src/index.js
@@ -5,21 +5,16 @@ import Traveler from './Traveler.js'
 import Destination from './Destination.js'
 import Trip from './Trip.js'
 
-// QUERY SELECTORS
-const welcomeText = document.querySelector("#welcome-text")
-const cardGrid = document.querySelector(".card-grid")
-const buttonQuote = document.querySelector("#button-quote")
-const buttonSubmit = document.querySelector("#button-submit")
+const welcomeText = document.querySelector('#welcome-text')
+const cardGrid = document.querySelector('.card-grid')
+const buttonQuote = document.querySelector('#button-quote')
+const buttonSubmit = document.querySelector('#button-submit')
 
-// EVENT LISTENERS
 // buttonQuote.addEventListener('click', tbd)
 // buttonSubmit.addEventListener('click', tbd)
 
-// GLOBAL VARIABLES
 let currentTraveler
-// const travelerTripDestinations = []
 
-// FETCH API DATASETS
 const fetchedTravelerData = fetchApi.getTravelerData()
 const fetchedTripData = fetchApi.getTripData()
 const fetchedDestinationData = fetchApi.getDestinationData()
@@ -27,54 +22,47 @@ const fetchedDestinationData = fetchApi.getDestinationData()
 Promise.all([fetchedTravelerData, fetchedTripData, fetchedDestinationData])
   .then(values => {
     generateTraveler(values[0])
-    findTrips(values[1])
-    // generateDestinations(values[2])
-    // loadWebsite()
-  }).catch(handleErrorMessages)
-
-// function loadWebsite() {
-//   createTripCards()
-// }
+    findTravelerTrips(values[1])
+    matchTripDestinations(values[2])
+    createTripCards()
+  })
+  .catch(handleErrorMessages)
 
 function handleErrorMessages(error) {
   // window.alert('The server was not accessible at this time. Please reload the page or try again later.')
   console.log(error)
 }
 
-// POPULATE WEBSITE INFORMATION
 function generateTraveler(travelerData) {
   currentTraveler = new Traveler(travelerData.travelers[Math.floor(Math.random() * travelerData.travelers.length)])
   let firstName = currentTraveler.name.split(' ')[0]
   domUpdates.addWelcomeMessage(firstName)
-  // findTravelerTrips(currentTraveler.id)
 }
 
-function findTrips(tripData) {
+function findTravelerTrips(tripData) {
   tripData.trips.forEach(trip => {
     let travelerTrip = new Trip(trip)
+
     if (travelerTrip.userId === currentTraveler.id) {
       currentTraveler.trips.push(travelerTrip)
     }
   })
 }
 
-// function generateDestinations(destinationData) {
-//   destinationData.destinations.forEach(destination => {
-//     let tripDestination = new Destination(destination)
-//     if (trip.userID === currentTraveler.id) {
-//       travelerTrips.push(trip)
-//     }
-//   })
-//   console.log(travelerTrips);
-// }
+function matchTripDestinations(destinationData) {
+  destinationData.destinations.forEach(location => {
+    let tripDestination = new Destination(location)
 
-// function findTravelerTrips(travelerId) {
-//   travelerTrips = fetchedTripData.trips.filter(trip => trip.userID === travelerId)
-//   console.log(travelerTrips)
-// }
+    currentTraveler.trips.forEach(trip => {
+      if (trip.destinationId === tripDestination.id) {
+        trip.destinationDetails = tripDestination
+      }
+    })
+  })
+}
 
-// function createTripCards() {
-//   fetchedTripData.forEach(trip => {
-//     domUpdates.addCardToDom(trip)
-//   })
-// }
+function createTripCards() {
+  currentTraveler.trips.forEach(trip => {
+    domUpdates.addCardToDom(trip)
+  })
+}
